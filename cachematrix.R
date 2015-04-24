a723daa91322411d6f14aed243588bce85f1e0ff
Sergey Nazarov <renew4@yandex.ru>
2015-04-24 15:06:38 +0300

    filled with functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create for a given matrix "x" a special object (a list) with 
+## 4 functions which set, store and return the matrix itself and invers matrix
+## Check if invers matrix for the given matrix already exists
+## return solution if it was earlier calculeted, 
+## calculate solution and store it if matrix "x" is first time here 
 
-## Write a short comment describing this function
+## this function has as input matrix "x"
+## as output a special object (a list) with 4 functions 
+## which set, store and return the matrix "x" and the invers matrix "s"   
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set=set, get=get, 
+             setsolve=setsolve,
+             getsolve=getsolve)
 }
 
+## "s" is an invers matrix for "x" 
+## This funtion checks if invers matrix for the given matrix already exists in 
+## previouse function 
+## returns solution if it was earlier calculeted, 
+## calculate solution if this matrix is first time here 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
